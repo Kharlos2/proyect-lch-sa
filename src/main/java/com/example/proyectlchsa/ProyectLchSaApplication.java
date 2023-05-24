238241100e8262ff09e75710d3a0914512723422
Luis Florez <braian1k3@gmail.com>
2023-05-24 11:38:42 -0500

    Crear proyecto y establecer conexión con la DB.

---
+++ src/main/java/com/example/proyectlchsa/ProyectLchSaApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectlchsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectLchSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectLchSaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectlchsa/ProyectLchSaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectlchsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectLchSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
