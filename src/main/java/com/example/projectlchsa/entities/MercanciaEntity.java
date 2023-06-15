8b9a1574b051aa632bb90fc87882c7e6f109406a
Luis Florez <braian1k3@gmail.com>
2023-06-15 11:59:12 -0500

    Agregarle los setters a MercanciaEntity.

--- src/main/java/com/example/projectlchsa/entities/MercanciaEntity.java
+++ src/main/java/com/example/projectlchsa/entities/MercanciaEntity.java
@@ -37,27 +37,55 @@ public class MercanciaEntity {
         return iup;
     }
 
+    public void setIup(Long iup) {
+        this.iup = iup;
+    }
+
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getDescripcion() {
         return descripcion;
     }
 
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
     public LocalDate getFechaEntradaBodega() {
         return fechaEntradaBodega;
+    }
+
+    public void setFechaEntradaBodega(LocalDate fechaEntradaBodega) {
+        this.fechaEntradaBodega = fechaEntradaBodega;
     }
 
     public String getMotivoDevolucion() {
         return motivoDevolucion;
     }
 
+    public void setMotivoDevolucion(String motivoDevolucion) {
+        this.motivoDevolucion = motivoDevolucion;
+    }
+
     public Double getVolumen() {
         return volumen;
     }
 
+    public void setVolumen(Double volumen) {
+        this.volumen = volumen;
+    }
+
     public ZonaEntity getZona() {
         return zona;
+    }
+
+    public void setZona(ZonaEntity zona) {
+        this.zona = zona;
     }
 }
