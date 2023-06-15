d84840415720636a7488d77c9fd1d685a5baf683
Luis Florez <braian1k3@gmail.com>
2023-06-15 09:03:59 -0500

    Generar nuevos metodos para el mapeo de ZonaEntity - ZonaRequestDTO.

--- src/main/java/com/example/projectlchsa/mappers/ZonaMapper.java
+++ src/main/java/com/example/projectlchsa/mappers/ZonaMapper.java
@@ -1,9 +1,13 @@
 package com.example.projectlchsa.mappers;
 
 import com.example.projectlchsa.dtos.zona.ZonaCorrectoDTO;
+import com.example.projectlchsa.dtos.zona.ZonaRequestDTO;
 import com.example.projectlchsa.entities.ZonaEntity;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
 import java.util.List;
 
 @Mapper(componentModel = "spring")
@@ -15,4 +19,19 @@ public interface ZonaMapper {
     @InheritInverseConfiguration
     ZonaEntity zonaEntidad(ZonaCorrectoDTO zonaCorrectoDTO);
     List<ZonaEntity> zonasEntidades(List<ZonaCorrectoDTO> zonaCorrectoDTOS);
+
+    @Mappings({
+            @Mapping(source = "espacioTotal", target = "total"),
+            @Mapping(source = "espacioParcial", target = "parcial")
+    })
+    ZonaRequestDTO zonaPeticionDTO(ZonaEntity zonaEntity);
+    List<ZonaRequestDTO> zonasPeticionesDTOs(List<ZonaEntity> zonaEntities);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "mercanciaEntities", ignore = true)
+    })
+    ZonaEntity zonaEntidadRequest(ZonaRequestDTO zonaRequestDTO);
+    List<ZonaEntity> zonasEntidadesRequest(List<ZonaRequestDTO> zonaRequestDTOS);
 }
